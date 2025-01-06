566f455ae3719c7b37fc9003dea104239cad3c60
Armiron Kurbalaj <armironkurbalaj@gmail.com>
2025-01-06 15:57:28 +0100

    Added location, and detail page code, theres need for API_KEY to be replaced withing the project, now in an internal not committed .env file

--- lib/screens/add_place_screen.dart
+++ lib/screens/add_place_screen.dart
@@ -1,9 +1,11 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import '../models/place.dart';
 import '../widgets/image_input.dart';
 import 'package:provider/provider.dart';
 import '../providers/great_places.dart';
+import '../widgets/location_input.dart';
 
 class AddPlaceScreen extends StatefulWidget {
   static const routeName = '/add-place';
@@ -15,17 +17,24 @@ class AddPlaceScreen extends StatefulWidget {
 class _AddPlaceScreenState extends State<AddPlaceScreen> {
   final _titleController = TextEditingController();
   File _pickedImage = File('');
+  PlaceLocation? _pickedLocation;
 
   void _selectImage(File pickedImage) {
     _pickedImage = pickedImage;
   }
 
+  void _selectPlace(double lat, double lng) {
+    _pickedLocation = PlaceLocation(latitude: lat, longitude: lng);
+  }
+
   void _savePlace() {
-    if (_titleController.text.isEmpty || _pickedImage.path.isEmpty) {
+    if (_titleController.text.isEmpty ||
+        _pickedImage.path.isEmpty ||
+        _pickedLocation == null) {
       return;
     }
     Provider.of<GreatPlaces>(context, listen: false)
-        .addPlace(_titleController.text, _pickedImage);
+        .addPlace(_titleController.text, _pickedImage, _pickedLocation!);
     Navigator.of(context).pop();
   }
 
@@ -50,6 +59,8 @@ class _AddPlaceScreenState extends State<AddPlaceScreen> {
                     ),
                     SizedBox(height: 10),
                     ImageInput(_selectImage),
+                    SizedBox(height: 10),
+                    LocationInput(_selectPlace),
                   ],
                 ),
               ),
--- lib/screens/places_list_screen.dart
+++ lib/screens/places_list_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import '../screens/place_detail_screen.dart';
 import 'package:provider/provider.dart';
 import '../screens/add_place_screen.dart';
 import '../providers/great_places.dart';
@@ -42,8 +43,12 @@ class PlacesListScreen extends StatelessWidget {
                                 FileImage(greatPlaces.items[i].image),
                           ),
                           title: Text(greatPlaces.items[i].title),
+                          subtitle: Text(greatPlaces.items[i].location.address),
                           onTap: () {
-                            // Go to detail page ...
+                            Navigator.of(context).pushNamed(
+                              PlaceDetailScreen.routeName,
+                              arguments: greatPlaces.items[i].id,
+                            );
                           },
                         ),
                       ),
@@ -51,7 +56,7 @@ class PlacesListScreen extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          // Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
+          Navigator.of(context).pushNamed(AddPlaceScreen.routeName);
         },
         child: const Icon(Icons.add),
       ),
